6ef7d9e582363d75bb97d7b899e579c58b2d3cfe
krishgour789 <krishgour789@gmail.com>
2026-05-15 19:56:05 +0530

    Creating a new file

---
+++ Day1.py
@@ -0,0 +1,23 @@
+# my_array = [1,2,3,5,4,5] # array of numbers
+# minVal = my_array[0]     # initialize minVal to the first element of the array
+# for i in my_array:       # loop through each element in the array
+#     if i < minVal:       # if the current element is less than minVal, update minVal
+#         minVal = i
+# print(minVal)
+
+# Find the Fibonacci series using the loop
+
+# prev2 = 0
+# prev1 = 1
+# print(prev2)
+# print(prev1)
+# for i in range(18):
+#     newfibo = prev2 + prev1
+#     print(newfibo)
+#     prev2= prev1
+#     prev1 = newfibo
+
+
+my_array = [8,3,5,4,]
+minVal = my_array[0]
+print(minVal)
